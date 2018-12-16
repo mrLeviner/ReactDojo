39a3614e85a4a23e16f7109dad7974d4a4a1c34d
mrLeviner <mrLeviner@gmail.com>
2018-12-16 06:01:27 -0500

    Add react-typist and turn pulse animation on

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Typist from 'react-typist';
 import Countdown from './Countdown.js';
 import prettymomma from './pretty_momma.png';
 import heartbeat from './heartbeat.jpeg';
@@ -8,11 +9,7 @@ import './fonts/font-awesome-4.2.0/css/font-awesome.min.css';
 // import SideBar from './sidebar';
 // import SemanticSidebar from './SemanticSidebar.js';
 
-/* If the use of global variables defined in script tags in html files
-  is desired, you can circumvent the linter by reading the variable
-  explicitly from the window object (ex: const $ = window.$;).
-  Alternatively, you can force the linter to ignore any line by adding
-  the comment // eslint-disable-line after it. */
+// Force linter to ignore a line by adding the comment // eslint-disable-line after it.
 
 class App extends Component {
   render() {
@@ -22,11 +19,15 @@ class App extends Component {
         {/* <SideBar pageWrapId={'page-wrap'} outerContainerId={'App'} /> */}
         <div id="page-wrap">
           <header className="App-header">
-            <img src={prettymomma} className="App-logo" alt="logo" />{' '}
-            {/* pulse */}
+            <img src={prettymomma} className="App-logo pulse" alt="logo" />
             <img src={heartbeat} className="App-heartbeat" alt="heartbeat" />
             <div className="title">
-              <em>Baby Leviner is due on May 31, 2019, in only</em>
+              <Typist>
+                <em>
+                  The due date for Baby Leviner is May 31, 2019! The countdown
+                  is on and she will arrive in...
+                </em>
+              </Typist>
             </div>
             <Countdown date={`2019-05-31T00:00:00`} />
           </header>
