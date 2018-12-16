a0697b267249a525d77914644d8be9a89e19d72b
mrLeviner <mrLeviner@gmail.com>
2018-12-16 04:51:48 -0500

    Comment incomplete code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.scss';
 import './styles.css';
 import './fonts/font-awesome-4.2.0/css/font-awesome.min.css';
 // import SideBar from './sidebar';
-import SemanticSidebar from './SemanticSidebar.js';
+// import SemanticSidebar from './SemanticSidebar.js';
 
 /* If the use of global variables defined in script tags in html files
   is desired, you can circumvent the linter by reading the variable
@@ -16,20 +16,23 @@ import SemanticSidebar from './SemanticSidebar.js';
 
 class App extends Component {
   render() {
-    return <div id="app" className="App">
-        <SemanticSidebar />
+    return (
+      <div id="app" className="App">
+        {/* <SemanticSidebar /> */}
         {/* <SideBar pageWrapId={'page-wrap'} outerContainerId={'App'} /> */}
         <div id="page-wrap">
           <header className="App-header">
-            <img src={ prettymomma } className="App-logo" alt="logo" /> {/* pulse */}
-            <img src={ heartbeat } className="App-heartbeat" alt="heartbeat" />
+            <img src={prettymomma} className="App-logo" alt="logo" />{' '}
+            {/* pulse */}
+            <img src={heartbeat} className="App-heartbeat" alt="heartbeat" />
             <div className="title">
               <em>Baby Leviner is due on May 31, 2019, in only</em>
             </div>
             <Countdown date={`2019-05-31T00:00:00`} />
           </header>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
