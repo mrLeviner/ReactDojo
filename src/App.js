3bc96662acc9c4bfd50da8f6a9612b2435abc8a3
mrLeviner <mrLeviner@gmail.com>
2018-12-27 07:18:51 -0500

    Delete unused files
    Install react-sidebar node package
    
    Replace tab logo
    
    Add components to build sidenav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import Sidebar from 'react-sidebar';
 import Typist from 'react-typist';
+import MaterialTitlePanel from './MaterialTitlePanel';
+import SidebarContent from './SidebarContent';
 import Countdown from './Countdown.js';
 import prettymomma from './assets/images/pretty_momma.png';
-import heartbeat from './assets/images/heartbeat.jpeg';
+// import heartbeat from './assets/images/heartbeat.jpeg';
 import './assets/styles/App.scss';
-import './assets/styles/styles.css';
-import './assets/fonts/font-awesome-4.2.0/css/font-awesome.min.css';
-// import SideBar from './sidebar';
-// import SemanticSidebar from './SemanticSidebar.js';
+
+const mql = window.matchMedia(`(min-width: 1400px)`);
+const styles = {
+  contentHeaderMenuLink: {
+    textDecoration: 'none',
+    color: 'white',
+    float: 'right',
+    verticalAlign: 'middle'
+  },
+  content: {
+    // padding: '16px'
+  }
+};
 
 /*
   - Run npm-home {package-name} to view the package homepage. Add -g to view
@@ -16,28 +28,95 @@ import './assets/fonts/font-awesome-4.2.0/css/font-awesome.min.css';
  */
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      docked: mql.matches,
+      open: false
+    };
+
+    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+    this.onSetOpen = this.onSetOpen.bind(this);
+  }
+
+  componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+  }
+
+  componentWillUnmount() {
+    mql.removeListener(this.mediaQueryChanged);
+  }
+
+  onSetOpen(open) {
+    this.setState({ open: open });
+  }
+
+  mediaQueryChanged() {
+    this.setState({ docked: mql.matches, open: false });
+  }
+
+  toggleOpen(event) {
+    this.setState({ open: !this.state.open });
+
+    if (event) {
+      event.preventDefault();
+    }
+  }
+
   render() {
+    const sidebar = <SidebarContent />;
+
+    const contentHeader = (
+      <span>
+        <span>Sullivan Raye Leviner</span>
+        {!this.state.docked && (
+          <a
+            onClick={this.toggleOpen}
+            href="https://github.com/balloob/react-sidebar"
+            style={styles.contentHeaderMenuLink}>
+            =
+          </a>
+        )}
+      </span>
+    );
+
+    const sidebarProps = {
+      sidebar,
+      docked: this.state.docked,
+      open: this.state.open,
+      onSetOpen: this.onSetOpen,
+      pullRight: true
+    };
+
     return (
-      <div id="app" className="App">
-        {/* <SemanticSidebar /> */}
-        {/* <SideBar pageWrapId={'page-wrap'} outerContainerId={'App'} /> */}
-        <div id="page-wrap">
-          <header className="App-header">
-            <img src={prettymomma} className="App-logo " alt="logo" />
-            {/* <img src={prettymomma} className="App-logo pulse" alt="logo" /> */}
-            <img src={heartbeat} className="App-heartbeat" alt="heartbeat" />
-            <div className="title">
-              <Typist>
-                <em>
-                  The due date for Baby Leviner is May 31, 2019! The countdown
-                  is on and she will arrive in...
-                </em>
-              </Typist>
+      <Sidebar {...sidebarProps}>
+        <MaterialTitlePanel title={contentHeader}>
+          <div id="app" className="App" style={styles.content}>
+            <div id="page-wrap">
+              <header className="App-header">
+                <img src={prettymomma} className="App-logo " alt="logo" />
+                {/* <img src={prettymomma} className="App-logo pulse" alt="logo" /> */}
+                {/* <img
+                  src={heartbeat}
+                  className="App-heartbeat"
+                  alt="heartbeat"
+                /> */}
+                <div className="title">
+                  <Typist>
+                    <em>
+                      The due date for Baby Leviner is May 31, 2019! The
+                      countdown is on and she will arrive in...
+                    </em>
+                  </Typist>
+                </div>
+                <Countdown date={`2019-05-31T00:00:00`} />
+              </header>
             </div>
-            <Countdown date={`2019-05-31T00:00:00`} />
-          </header>
-        </div>
-      </div>
+          </div>
+        </MaterialTitlePanel>
+      </Sidebar>
     );
   }
 }
